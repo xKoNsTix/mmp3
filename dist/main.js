0ec4ed51aaee72965cd9cfbab3f45e177ea8b376
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2024-02-25 17:18:52 +0100

    dist is now unignored

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var n;function c(){clearInterval(n),n=setInterval(function(){for(var e=document.getElementsByName("carousel"),t=0;t<e.length;t++)if(e[t].checked){e[t].checked=!1,t+1==e.length?e[0].checked=!0:e[t+1].checked=!0;return}},5e3)}c();})();
