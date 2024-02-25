f20d1138a507acbf0895ba3507f89cbc38112410
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2024-02-25 17:14:21 +0100

    edited deployscript

--- deploy.php
+++ deploy.php
@@ -1,48 +1,66 @@
 <?php
 namespace Deployer;
-
 require 'recipe/common.php';
 
-// Repository
-set('repository', 'git@github.com:xKoNsTix/mmp3.git');
+set('repository','git@github.com:xKoNsTix/mmp3.git');
 
-// Keep the last 3 releases
+// [Optional] Allocate tty for git clone. Default value is false.
+//set('git_tty', true);
+// Shared files/dirs between deploys
+
+
+//Before WP
 set('keep_releases', 3);
 
+
+// Writable dirs by web server
+set('writable_mode', 'chown');
+// set('writable_dirs', ['public/wp-content/uploads']);
+set('allow_anonymous_stats', false);
+
 // Hosts
-host('88.198.150.108')
-    ->set('remote_user', 'ex')
-    ->set('deploy_path', '/var/www/ex/dist'); // Deploy directly into the dist directory
+host('Lillesand')
+        ->set('remote_user','ex')
 
-// Define shared files and directories.
-// As it's a static site, you might not need any.
-// Just an example if later you have some shared resources.
-// set('shared_files', []);
-// set('shared_dirs', []);
 
-// Define writable dirs.
-// Static sites usually don't need this, but it's here if you need it.
-// set('writable_dirs', []);
+        ->set('deploy_path', '/var/www/ex');
+        //->set('deploy_path', '~/app');
 
-// Disable Composer action since it's a static HTML site
+// Composer
 set('composer_action', false);
 
-// Main task for deployment
+
+
+task('post-deploy', function () {
+
+    run('sudo chown -R ex:www-data /var/www/ex');
+    run('sudo chmod -R u+rwx /var/www/ex');
+  });
+
+
+// Tasks
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
+    //'deploy:info',
     'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    // 'deploy:shared', // Uncomment if you have shared resources
-    // 'deploy:writable', // Uncomment if needed
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+    //'deploy:lock',
+    //'deploy:release',
+   // 'deploy:update_code',
+    //'deploy:shared',
+    //'deploy:writable',
+    //'deploy:clear_paths',
+    //'deploy:symlink',
+    //'deploy:unlock',
+    'post-deploy',
+    'deploy:publish'
+
 ]);
 
-// [Optional] If deploy fails, automatically unlock.
+task('delete_src_folder', function () {
+        run('rm -rf {{release_path}}/src');
+    });
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy', 'delete_src_folder');
+
 after('deploy:failed', 'deploy:unlock');
